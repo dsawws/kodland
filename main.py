d8a479e3dc1e6e1f2168bf75680fa93c4819d4e8
NKVDshnik <118619951+dsawws@users.noreply.github.com>
2023-04-08 21:10:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
-import settings
+#from settings import settings
 import discord
+from discord.ext import commands
+
 import random
+# import * - это тоже самое, что перечислить все файлы
+#from bot_logic import *
 
+# Переменная intents - хранит привилегии бота
 intents = discord.Intents.default()
-
+# Включаем привелегию на чтение сообщений
 intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-
-# Когда бот будет готов, он напишет в консоли свое название!
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-
-# Когда бот будет получать сообщение, он будет отправлять в этот же канал какие-то сообщения!
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Привет! Я бот!')
-    elif message.content.startswith('$smile'):
-        await message.channel.send(gen_emodji())
-    elif message.content.startswith('$coin'):
-        await message.channel.send(flip_coin())
-    elif message.content.startswith('$pass'):
-        await message.channel.send(gen_pass(10))
-    elif message.content.startswith('$num'):
-        await message.channel.send(num(10))
-    else:
-        await message.channel.send("Я не понимаю такую команду!")
-
-
-
-
-
+# Создаем бота в переменной client и передаем все привелегии
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 def gen_pass(pass_length):
     elements = "+-/*!&$#?=@<>"
@@ -60,9 +35,25 @@ def flip_coin():
     else:
         return "РЕШКА"
     
-def num ():
-    number=random.randint(0, 10)
-    print(number)
+# Когда бот будет готов, он напишет в консоли свое название!
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
 
- 
-client.run(settings["TOKEN"])
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def smile(ctx):
+    await ctx.send(gen_emodji())
+
+@bot.command()
+async def coin(ctx):
+    await ctx.send(flip_coin())
+
+@bot.command()
+async def password(ctx, count = 10):
+    await ctx.send(gen_pass(count))
+
+bot.run("")
